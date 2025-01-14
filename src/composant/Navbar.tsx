e039f62322a042e4b9b73ff66a6fd2da2b8a04a4
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2025-01-14 11:31:40 +0100

    [ADD] Navbar

--- src/composant/Navbar.tsx
+++ src/composant/Navbar.tsx
@@ -1,26 +1,24 @@
 import { Link } from 'react-router-dom';
+import photo from '@/assets/Photo.jpg';
 
 function Navbar() {
-  return (
-    <header className="fixed top-0 w-full bg-gray-800 text-white shadow-md z-50">
-      <nav>
-        <ul className="flex justify-around items-center p-4">
-          <li className="hover:bg-gray-700 rounded-md px-4 py-2">
-            <Link to="/" className="text-lg">Home</Link>
-          </li>
-          <li className="hover:bg-gray-700 rounded-md px-4 py-2">
-            <Link to="/about" className="text-lg">About</Link>
-          </li>
-          <li className="hover:bg-gray-700 rounded-md px-4 py-2">
-            <Link to="/projects" className="text-lg">Projects</Link>
-          </li>
-          <li className="hover:bg-gray-700 rounded-md px-4 py-2">
-            <Link to="/contact" className="text-lg">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+    const classNameLink = "text-white p-3 hover:bg-slate-200 hover:bg-foret transition duration-200 ease-in-out w-full text-center h-full w-full";
+
+    return (
+        <div className="fixed top-0 left-0 right-0 bg-foret text-white z-50 shadow-[0_0_18px_4px_rgba(255,255,255,0.2)] text-xl font-bold">
+            <div className="flex justify-between items-center px-4 relative z-10 h-full">
+                <div className="flex gap-2 items-center p-2">
+                    <img src={photo} className="w-10 h-10 rounded-full" alt="A.JACQUEMET" /><p>A.JACQUEMET</p>
+                </div>
+                <div className="flex h-full">
+                    <Link className={classNameLink} to="/">Home</Link>
+                    <Link className={classNameLink} to="/about">About</Link>
+                    <Link className={classNameLink} to="/projects">Projects</Link>
+                    <Link className={classNameLink} to="/contact">Contact</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Navbar;
