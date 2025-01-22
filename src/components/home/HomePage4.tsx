09e86044c0ac174d9f4635f7265da173e6c7cb60
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2025-01-22 19:39:14 +0100

    [ADD] WIP clean Timeline

--- src/components/home/HomePage4.tsx
+++ src/components/home/HomePage4.tsx
@@ -1,69 +1,221 @@
-import { motion } from "framer-motion";
-import LineGradient from "../LineGradient";
+// import { motion } from "framer-motion";
+// import LineGradient from "../LineGradient";
 
-interface TimeLineElementProps {
-    title : string
-    time : string
-    children : React.ReactNode
-}
+// interface TimeLineElementProps {
+//     title : string
+//     time : string
+//     children : React.ReactNode
+// }
 
-function TimeLineElement({ title, time, children }: TimeLineElementProps) {
-    return (
-        <motion.div
-            initial={{ y: 50, x: 50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1, ease: 'backInOut' }}
-            className="relative md:w-1/2"
-        >
-            <div className="border bg-white/20 border-otherDarkBlue rounded-xl p-4 text-yellow flex flex-col">
-                <h3 className="text-xl font-semibold self-center">{title}</h3>
-                <span className="italic mb-2">{time}</span>
-                <p>{children}</p>
-            </div>
-        </motion.div>
-    )
-}
-function HomePage4() {
+// function TimeLineElement({ title, time, children }: TimeLineElementProps) {
+//     return (
+//         <motion.div
+//             initial={{ y: 50, x: 50, opacity: 0 }}
+//             animate={{ y: 0, opacity: 1 }}
+//             transition={{ duration: 1, ease: 'backInOut' }}
+//             className="relative md:w-1/2"
+//         >
+//             <div className="border bg-white/20 border-otherDarkBlue rounded-xl p-4 text-yellow flex flex-col">
+//                 <h3 className="text-xl font-semibold self-center">{title}</h3>
+//                 <span className="italic mb-2">{time}</span>
+//                 <p>{children}</p>
+//             </div>
+//         </motion.div>
+//     )
+// }
+// function HomePage4() {
 
-    return (
-        <div className="min-h-screen pt-10 pb-24 p-20">
-            <div className="md:flex md:justify-between md:gap-16 mt-32">
-                <motion.div
-                    className="md:w-1/3"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}
-                >
-                    <p className="font-playfair font-semibold text-4xl mb-5">
-                        MY <span className="text-accent">PROFESSIONNAL</span> EXPERIENCES
-                    </p>
-                    <LineGradient width="w-1/3" />
-                </motion.div>
-            </div>
-            <div className="relative flex flex-col gap-12 mt-16 items-center">
+//     return (
+//         <div className="min-h-screen pt-10 pb-24 p-20">
+//             <div className="md:flex md:justify-between md:gap-16 mt-32">
+//                 <motion.div
+//                     className="md:w-1/3"
+//                     initial="hidden"
+//                     whileInView="visible"
+//                     viewport={{ once: true, amount: 0.5 }}
+//                     transition={{ duration: 0.5 }}
+//                     variants={{
+//                         hidden: { opacity: 0, x: -50 },
+//                         visible: { opacity: 1, x: 0 },
+//                     }}
+//                 >
+//                     <p className="font-playfair font-semibold text-4xl mb-5">
+//                         MY <span className="text-accent">PROFESSIONNAL</span> EXPERIENCES
+//                     </p>
+//                     <LineGradient width="w-1/3" />
+//                 </motion.div>
+//             </div>
+//             <div className="relative flex flex-col gap-12 mt-16 items-center">
                 
-                <TimeLineElement title="Safran, France, July 2023" time="6 months">
-                    <>
-                        <span style={{ display: 'block', marginLeft: 0 }}>Coding in XSLT and JS: </span>
-                        <span style={{ display: 'block', marginLeft: 20 }}>• Creation an internal app to display XML data correctly</span>
-                        <span style={{ display: 'block', marginLeft: 20 }}>• Transform XML document to other format</span>
-                    </>
-                </TimeLineElement>
-                <TimeLineElement title="AI Hackathon with Extia, Barcelona - 05/24" time="2 days">
-                    <>
-                        <span style={{ display: 'block', marginLeft: 0 }}>Participation in an AI Hackathon with Extia in Barcelona.
-                            Creation of an AI model to help users to properly code.
-                        </span>
-                    </>
-                </TimeLineElement>
+//                 <TimeLineElement title="Safran, France, July 2023" time="6 months">
+//                     <>
+//                         <span style={{ display: 'block', marginLeft: 0 }}>Coding in XSLT and JS: </span>
+//                         <span style={{ display: 'block', marginLeft: 20 }}>• Creation an internal app to display XML data correctly</span>
+//                         <span style={{ display: 'block', marginLeft: 20 }}>• Transform XML document to other format</span>
+//                     </>
+//                 </TimeLineElement>
+//                 <TimeLineElement title="AI Hackathon with Extia, Barcelona - 05/24" time="2 days">
+//                     <>
+//                         <span style={{ display: 'block', marginLeft: 0 }}>Participation in an AI Hackathon with Extia in Barcelona.
+//                             Creation of an AI model to help users to properly code.
+//                         </span>
+//                     </>
+//                 </TimeLineElement>
+//             </div>
+//         </div>
+//     )
+// }
+
+import { Timeline } from "../ui/timeline";
+
+function HomePage4() {
+    const data = [
+        {
+          title: "2024",
+          content: (
+            <div>
+              <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+                Built and launched Aceternity UI and Aceternity UI Pro from scratch
+              </p>
+              <div className="grid grid-cols-2 gap-4">
+                <img
+                  src="https://assets.aceternity.com/templates/startup-1.webp"
+                  alt="startup template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/templates/startup-2.webp"
+                  alt="startup template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/templates/startup-3.webp"
+                  alt="startup template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/templates/startup-4.webp"
+                  alt="startup template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+              </div>
             </div>
-        </div>
-    )
+          ),
+        },
+        {
+          title: "Early 2023",
+          content: (
+            <div>
+              <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+                I usually run out of copy, but when I see content this big, I try to
+                integrate lorem ipsum.
+              </p>
+              <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+                Lorem ipsum is for people who are too lazy to write copy. But we are
+                not. Here are some more example of beautiful designs I built.
+              </p>
+              <div className="grid grid-cols-2 gap-4">
+                <img
+                  src="https://assets.aceternity.com/pro/hero-sections.png"
+                  alt="hero template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/features-section.png"
+                  alt="feature template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/pro/bento-grids.png"
+                  alt="bento template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/cards.png"
+                  alt="cards template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+              </div>
+            </div>
+          ),
+        },
+        {
+          title: "Changelog",
+          content: (
+            <div>
+              <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+                Deployed 5 new components on Aceternity today
+              </p>
+              <div className="mb-8">
+                <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+                  ✅ Card grid component
+                </div>
+                <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+                  ✅ Startup template Aceternity
+                </div>
+                <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+                  ✅ Random file upload lol
+                </div>
+                <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+                  ✅ Himesh Reshammiya Music CD
+                </div>
+                <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+                  ✅ Salman Bhai Fan Club registrations open
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <img
+                  src="https://assets.aceternity.com/pro/hero-sections.png"
+                  alt="hero template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/features-section.png"
+                  alt="feature template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/pro/bento-grids.png"
+                  alt="bento template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+                <img
+                  src="https://assets.aceternity.com/cards.png"
+                  alt="cards template"
+                  width={500}
+                  height={500}
+                  className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+              </div>
+            </div>
+          ),
+        },
+      ];
+      return (
+            <Timeline data={data} />
+      );
 }
 
 export default HomePage4
