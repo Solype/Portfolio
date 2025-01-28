4fc903bc125c79f0c5f8645f492c0e6f034711ab
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2025-01-28 19:15:39 +0100

    [ADD] images to the timeline

--- src/components/home/HomePage4.tsx
+++ src/components/home/HomePage4.tsx
@@ -1,6 +1,13 @@
 import { Timeline, TimelineEntry } from "../ui/timeline";
 import NDP_icon from "@/assets/NDP.png";
 import Epitech_icon from "@/assets/epitech.png";
+import Epitech_berlin1 from "@/assets/epitech_berlin1.jpg";
+import Epitech_berlin2 from "@/assets/epitech_berlin2.jpg";
+import Area_image1 from "@/assets/Area_image1.png";
+import RType_image1 from "@/assets/Rtype_image1.png";
+import EpitechBarcelona1 from "@/assets/Epitech_Barcelona1.jpg"
+import EpitechBarcelona2 from "@/assets/Epitech_Barcelona2.jpg"
+import ArcadeImage1 from "@/assets/Arcade1.png";
 
 function HomePage4() {
     const image_className = "rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]";
@@ -14,13 +21,13 @@ function HomePage4() {
                         Doing a lot of long project, I have learned how to work on a project that last several months.
                     </p>
                     <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        In this year, i have been able to work on projects like Area and RType, that you can discover in the projects section.
                     </div>
                     <div className="grid grid-cols-2 gap-4">
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
+                        <img src={Epitech_berlin1} alt="epitech berlin image 1" width={500} height={500} className={image_className}/>
+                        <img src={Epitech_berlin2} alt="epitech berlin image 2" width={500} height={500} className={image_className}/>
+                        <img src={Area_image1} alt="Area project image" width={500} height={500} className={image_className}/>
+                        <img src={RType_image1} alt="RType project image" width={500} height={500} className={image_className}/>
                     </div>
                 </div>
             )
@@ -34,12 +41,19 @@ function HomePage4() {
                         second year at Epitech Barcelona.
                     </p>
                     <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        <p>
+                            For the first time, in Barcelona, I studied abroad, far from France. However, I was able to
+                            learn a lot in this year. Especially the c++ language.
+                        </p>
+                        <p>
+                            I have been able to work on projects like Arcade, that you can discover in the projects
+                            section.
+                        </p>
                     </div>
                     <div className="grid grid-cols-2 gap-4">
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
-                        <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
+                        <img src={EpitechBarcelona1} alt="Epitech Barcelona image 1" width={500} height={500} className={image_className}/>
+                        <img src={EpitechBarcelona2} alt="Epitech Barcelona image 2" width={500} height={500} className={image_className}/>
+                        <img src={ArcadeImage1} alt="Arcade project image" width={500} height={500} className={image_className}/>
                         <img src={Epitech_icon} alt="hero template" width={500} height={500} className={image_className}/>
                     </div>
                 </div>
